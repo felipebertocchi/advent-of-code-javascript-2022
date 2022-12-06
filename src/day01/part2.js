93dc670f4712a7ae519b592f65112df06d404978
felipebertocchi <fabertocchi@gmail.com>
2022-12-06 01:00:02 -0300

    day 1

--- src/day01/part2.js
+++ src/day01/part2.js
@@ -1,5 +1,17 @@
 // Advent of Code - Day 1 - Part Two
 
 export function part2(input) {
-  return 0;
+  const splitInput = input.split(/\r?\n|\r|\n/g);
+  let mainArray = [];
+  let tempSum = 0
+  splitInput.forEach(el => {
+    if (el === "") {
+      mainArray.push(tempSum);
+      tempSum = 0
+    } else {
+      tempSum += (+el);
+    }
+  });
+  mainArray.sort(function(a, b){return b - a});
+  return mainArray[0] + mainArray[1] + mainArray[2] 
 }
